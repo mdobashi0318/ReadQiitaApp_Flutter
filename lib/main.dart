36576f26080ec0ca3efacf622f9ebcf374af13c0
MasaharuDobashi <masaharu.dobashi@gmail.com>
2023-07-02 22:27:30 +0900

    記事一覧のセルの表示更新

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:read_qiita_app_flutter/model/article.dart';
 
 import 'article_list_notifier.dart';
+import 'model/tags.dart';
 
 final articleProvider = FutureProvider<List<Article>>(
   (ref) {
@@ -55,23 +58,67 @@ class MyHomePage extends ConsumerWidget {
               final article = articles[index] as Article;
               return Container(
                 padding: const EdgeInsets.all(10),
-                child: Column(children: [
-                  Row(
-                    children: [
-                      Text('@${article.user.id}'),
-                      Text(article.user.organization ?? ""),
-                    ],
-                  ),
-                  Text(article.created_at),
-                  Text(
-                    article.title,
-                  )
-                ]),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Text('@${article.user.id}'),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        Flexible(child: Text(article.user.organization ?? "")),
+                      ],
+                    ),
+                    Text(
+                      article.created_at,
+                    ),
+                    Text(
+                      article.title,
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Transform.rotate(
+                          angle: 220 * pi / 180,
+                          child: const Icon(
+                            Icons.label,
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 5,
+                        ),
+                        Flexible(child: Text(tagNames(article.tags).join(','))),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        const Icon(Icons.favorite_border),
+                        const SizedBox(
+                          width: 5,
+                        ),
+                        Text(article.likes_count.toString())
+                      ],
+                    )
+                  ],
+                ),
               );
             },
           );
         },
       ),
     );
+  }
+
+  /// タグの名前の配列を返す
+  List<String> tagNames(List<Tags> tags) {
+    List<String> tagsNames = [];
+    for (var tag in tags) {
+      tagsNames.add(tag.name);
+    }
+    return tagsNames;
   }
 }
